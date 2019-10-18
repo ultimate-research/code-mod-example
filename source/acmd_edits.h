9276fd3ec8288aa05745e7f0dfba413bd04e6010
jugeeya <jugeeya@live.com>
2019-10-18 15:24:34 -0700

    update to latest framework and use corresponding namespaces

--- source/acmd_edits.h
+++ source/acmd_edits.h
@@ -10,6 +10,12 @@
 
 using namespace lib;
 using namespace app::sv_animcmd;
+using namespace app::sv_math;
+using namespace app::sv_kinetic_energy;
+using namespace app::sv_battle_object;
+using namespace app::sv_module_access;
+using namespace app::FL_sv_module_access;
+using namespace app::sv_system;
 using namespace app::lua_bind;
 
 ACMD acmd_objs[] = {
